a9451c584b634139a50be79b648d24bb4317f96e
abdurrahim220 <z.abdurrahim5@gmail.com>
2024-01-24 09:46:35 +0600

    some changes

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const propertyRouter = require('./routes/propertyRoute');
 app.use(cookieParser())
 
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors());
 
 
 app.use("/api/auth", userRouter);
